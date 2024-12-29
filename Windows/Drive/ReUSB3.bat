7ca1bbba47b9aef239dc4e50c4e24bc289848987
mujianwu <lrh15563910083@163.com>
2024-12-29 17:24:35 +0800

    Create ReUSB3.bat

---
+++ Windows/Drive/ReUSB3.bat
@@ -0,0 +1,3 @@
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "osvc" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "SkipContainerIdQuery" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100" /v "SkipBOSDescriptorQuery" /f
